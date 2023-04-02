3ff0b6c07ad880d8621826bd2a8029f533becdfe
maorlayani <maorlayani@gmail.com>
2023-04-02 10:45:51 +0300

    add tests to user reducer

---
+++ src/store/user.reducer.test.js
@@ -0,0 +1,23 @@
+import 'core-js'
+import { userReducer } from './user.reducer'
+
+describe('', () => {
+    const mockUser = { fullname: 'John Walls', username: 'Walls123', id: 'u101' }
+    const initialState = {
+        user: null,
+        users: [],
+        watchedUser: null
+    }
+    test('creates initial state', () => {
+        expect.assertions(1)
+        const state = userReducer(initialState)
+        expect(state).toBe(initialState)
+    })
+    test('should set a user in the state', () => {
+        expect.assertions(2)
+        let state = userReducer(initialState)
+        expect(state.user).toBeFalsy()
+        state = userReducer(initialState, { type: 'SET_USER', user: mockUser })
+        expect(state.user).toBeTruthy()
+    })
+})
